9a59d4d97e7a57e3f9bf51e8f75de3d512cb6fc9
Enzobulacio18@outlook.com <Enzobulacio18@outlook.com>
2021-10-01 00:06:22 -0300

    falta nlog

---
+++ practica/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using practica.Controllers;
+using Microsoft.AspNetCore.Http;
+using practica.Models;
+namespace practica.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Formulario(string nombre,string apellido,DateTime anio_nac,DateTime anio_ing,double salario) 
+        {
+            CargarEmpleado miEmpleado = new CargarEmpleado(nombre,apellido,anio_nac,anio_ing,salario);
+            return View(miEmpleado);
+        }
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ practica/Models/CargarEmpleado.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace practica.Models
+{
+    public class CargarEmpleado
+    {
+        private string nombre;
+        private string apellido;
+        private DateTime nacimiento;
+        private DateTime anioIngreso;
+        private double sueldo;
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public DateTime Nacimiento { get => nacimiento; set => nacimiento = value; }
+        public DateTime AnioIngreso { get => anioIngreso; set => anioIngreso = value; }
+        public double Sueldo { get => sueldo; set => sueldo = value; }
+
+        public CargarEmpleado(string nombre,string apellido,DateTime nacimiento,DateTime anioIngreso,double sueldo)
+        {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.Nacimiento = nacimiento;
+            this.AnioIngreso = anioIngreso;
+            this.Sueldo = sueldo + AdiccionalSalario(antiguedad()) - (sueldo * 0.15);
+
+        }
+       public int antiguedad()
+        {
+            int anti = (DateTime.Now - anioIngreso).Days / 365;
+            return anti;
+        }
+       
+        public double AdiccionalSalario(float anti)
+        {
+
+            if (anti<1)
+            {
+                return sueldo;
+            }
+            if (anti >= 1 && anti<=20)
+            {
+                return sueldo * anti / 100;
+            }
+            if (anti > 20)
+            {
+                return sueldo * 0.25;
+            }
+            else
+                return -999;
+                
+        }
+       
+    }
+}
